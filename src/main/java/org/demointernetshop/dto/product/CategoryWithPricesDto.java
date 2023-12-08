40c4ae50e6fc3c2398122960d152067acab16f8a
StanislavPB <perminovsb@gmail.com>
2023-12-08 12:13:56 +0000

    add  Controllers and services

--- src/main/java/org/demointernetshop/dto/product/CategoryWithPricesDto.java
+++ src/main/java/org/demointernetshop/dto/product/CategoryWithPricesDto.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,9 +15,9 @@ public class CategoryWithPricesDto {
     @Schema(description = "Category identifier", example = "1")
     private Integer id;
     @Schema(description = "minPrice", example = "201")
-    private Integer minPrice;
+    private BigDecimal minPrice;
     @Schema(description = "maxPrice", example = "10000")
-    private Integer maxPrice;
+    private BigDecimal maxPrice;
     @Schema(description = "Name of the Category", example = "smartphones")
     private String categoryName;
 }
--- src/main/java/org/demointernetshop/services/CategoryService.java
+++ src/main/java/org/demointernetshop/services/CategoryService.java
@@ -4,11 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.demointernetshop.dto.product.CategoryDto;
 import org.demointernetshop.dto.product.CategoryWithPricesDto;
 import org.demointernetshop.entity.Category;
+import org.demointernetshop.entity.ProductInfo;
 import org.demointernetshop.repository.CategoryRepository;
+import org.demointernetshop.repository.ProductInfoRepository;
 import org.demointernetshop.services.utils.Converters;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +23,7 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
     private final Converters converters;
+    private final ProductInfoRepository productInfoRepository;
 
     public List<CategoryDto> getAllCategory(){
         return categoryRepository.findAll().stream()
@@ -27,9 +32,34 @@ public class CategoryService {
     }
 
     public CategoryWithPricesDto getCategoryWithPrices(Integer categoryId) {
-        Optional<Category> categoryByIdOptional = categoryRepository.findById(categoryId);
-        if (categoryByIdOptional.isPresent()) {
-            Integer minPrice =
-         }
+       /*
+       1. Создать объект, который будет содержать id категории, минимальную цену в этой категории, максимальную цену товара в этой категории и название категории
+       2. Определить минимальную цену товаров в этой категории
+       3. Определить максимальную цену товаров в этой категории
+       4. Вытащить название категории из таблицы
+        */
+
+//        List<ProductInfo> productInfos = productInfoRepository.findAll();
+//
+//        BigDecimal minPrice = productInfos.stream()
+//                .filter(productInfo -> productInfo.getProduct().getCategory().getId().equals(categoryId))
+//                .map(productInfo -> productInfo.getPrice())
+//                .min(Comparator.naturalOrder())
+//                .orElse(null);
+//
+//        BigDecimal maxPrice = productInfos.stream()
+//                .filter(productInfo -> productInfo.getProduct().getCategory().getId().equals(categoryId))
+//                .map(productInfo -> productInfo.getPrice())
+//                .max(Comparator.naturalOrder())
+//                .orElse(null);
+//
+//        String categoryName = categoryRepository.findById(categoryId).get().getCategoryName();
+//
+//        return new CategoryWithPricesDto(categoryId,minPrice,maxPrice, categoryName);
+
+
+        return productInfoRepository.findPriceByCategory(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found or no products in the category"));
+
     }
 }

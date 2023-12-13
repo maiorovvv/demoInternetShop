a45b242d799232d47ff8e22c70b95a859cb2d9da
StanislavPB <perminovsb@gmail.com>
2023-12-13 12:04:47 +0000

    add  Controllers and services

--- src/main/java/org/demointernetshop/services/utils/ModelMapperConfig.java
+++ src/main/java/org/demointernetshop/services/utils/ModelMapperConfig.java
@@ -3,7 +3,7 @@ package org.demointernetshop.services.utils;
 
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
 public class ModelMapperConfig {
 //    public ModelMapper modelMapper() {
 //        ModelMapper modelMapper = new ModelMapper();

40587a8e735d7ad4ed3d7247b50be2fb416cfc30
StanislavPB <perminovsb@gmail.com>
2023-12-18 08:51:23 +0000

    add  Controllers and services

--- src/main/java/org/demointernetshop/controllers/exception/GlobalExceptionHandler.java
+++ src/main/java/org/demointernetshop/controllers/exception/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package org.demointernetshop.controllers.exception;
 
+import jakarta.mail.MessagingException;
 import jakarta.validation.ConstraintViolationException;
 import org.demointernetshop.services.exceptions.IsAlreadyExistException;
 import org.demointernetshop.services.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailAuthenticationException;
+import org.springframework.mail.MailParseException;
+import org.springframework.mail.MailSendException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.IOException;
 import java.sql.SQLIntegrityConstraintViolationException;
 
 public class GlobalExceptionHandler {
@@ -38,4 +43,36 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(MailAuthenticationException.class)
+    public ResponseEntity<String> handleMailAuthenticationException(MailAuthenticationException e){
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body("Mail Authentication Error");
+    }
+
+    @ExceptionHandler(MailSendException.class)
+    public ResponseEntity<String> handleMailSendException(MailSendException e){
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Mail Send Error");
+    }
+
+    @ExceptionHandler(MailParseException.class)
+    public ResponseEntity<String> handleMailParseException(MailParseException e){
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Mail Parse Error");
+    }
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<String> handleMessagingException(MessagingException e){
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Messaging Error");
+    }
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<String> handleIOException(IOException e){
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("IO Error");
+    }
 }

2f6531de0f1bdcf226a3094013207c10933aceeb
StanislavPB <perminovsb@gmail.com>
2023-12-18 08:57:27 +0000

    add  Controllers and services

--- src/main/java/org/demointernetshop/controllers/EmailController.java
+++ src/main/java/org/demointernetshop/controllers/EmailController.java
@@ -2,14 +2,15 @@ package org.demointernetshop.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.demointernetshop.entity.email.EmailRequest;
-import org.demointernetshop.mail.MailSender;
+
+import org.demointernetshop.mail.DemoInternetMailSender;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class EmailController {
-    private final MailSender mailSender;
+    private final DemoInternetMailSender mailSender;
 
     public String sendEmail(@RequestBody EmailRequest request){
         mailSender.send(request.getTo(), request.getSubject(), request.getText());

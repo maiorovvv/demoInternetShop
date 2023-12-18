febd30ca9e794f31ef3990a2892f8c41ffe241fe
StanislavPB <perminovsb@gmail.com>
2023-12-18 08:55:33 +0000

    add  Controllers and services

--- src/main/java/org/demointernetshop/mail/DemoInternetMailSender.java
+++ src/main/java/org/demointernetshop/mail/DemoInternetMailSender.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class MailSender {
+public class DemoInternetMailSender {
 
     private final JavaMailSender javaMailSender;
 
